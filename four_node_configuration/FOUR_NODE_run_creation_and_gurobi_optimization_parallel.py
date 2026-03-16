bdfe349f55a3d8449e0410b0ec699912576d862c
masseramatteo <m.massera@uu.nl>
2026-03-16 10:56:54 +0100

    using snellius changes

--- four_node_configuration/FOUR_NODE_run_creation_and_gurobi_optimization_parallel.py
+++ four_node_configuration/FOUR_NODE_run_creation_and_gurobi_optimization_parallel.py
@@ -826,7 +826,7 @@ if __name__ == "__main__":
         "hydrogen_import_price": [150, 200, 250, 300],
     }
 
-    n_samples_per_scenario = 30
+    n_samples_per_scenario = 3
 
     # ==========================================================================
     # 3) Build full combination list

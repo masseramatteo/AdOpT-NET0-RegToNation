513d251556482cfef2aa70c45593ea3b10b6e308
masseramatteo <m.massera@uu.nl>
2026-04-21 14:10:57 +0200

    Changes on large cluster limits

--- four_node_configuration/define_components_spec.py
+++ four_node_configuration/define_components_spec.py
@@ -59,6 +59,9 @@ def define_hydrogen_pipeline2(input_data_path):
     network_data["Performance"]["min_transport"] = 0
     network_data["Performance"]["loss"] = 0
 
+    network_data["Performance"]["pressure"]["hydrogen"]["inlet"] = 15
+    network_data["Performance"]["pressure"]["hydrogen"]["outlet"] = 15
+
     with open(input_data_path / "period1" / "network_data" / "hydrogenPipelineOnshore_lowP.json", "w") as json_file:
         json.dump(network_data, json_file, indent=4)
 
@@ -158,9 +161,20 @@ def define_electrolyzers(input_data_path):
                   "r") as json_file:
             electrolyzer_data = json.load(json_file)
 
-        electrolyzer_data["size_max"] = 400
+        electrolyzer_data["size_max"] = 150
 
         with open(input_data_path / "period1" / "node_data" / node / "technology_data" / "Electrolyzer_small.json",
                   "w") as json_file:
             json.dump(electrolyzer_data, json_file, indent=4)
+
+    for node in ["Small_cluster1", "Small_cluster2"]:
+        with open(input_data_path / "period1" / "node_data" / node / "technology_data" / "Photovoltaic.json",
+                  "r") as json_file:
+            photovoltaic_data = json.load(json_file)
+
+        photovoltaic_data["size_max"] = 50
+
+        with open(input_data_path / "period1" / "node_data" / node / "technology_data" / "Photovoltaic.json",
+                  "w") as json_file:
+            json.dump(photovoltaic_data, json_file, indent=4)
 
